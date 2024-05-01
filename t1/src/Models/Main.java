2afcd7e2a9b9bfa88195dd83d9e8a48b80d4bc23
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-05-01 17:08:36 -0300

    [fix] Main Models

--- t1/src/Models/Main.java
+++ t1/src/Models/Main.java
@@ -1,8 +1,6 @@
 package Models;
 
 public class Main {
-	Endereco[] endereco = new Endereco[100];
 	Fornecedor[] fornecedor = new Fornecedor[100];
 	Produto[] produto = new Produto[100];
-	Estoque[] estoque = new Estoque[100];
 }
