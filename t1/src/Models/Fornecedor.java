2e1cd7530cdc7dcfbf703a2ce0805986da8e300f
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-05-01 15:41:51 -0300

    [feat] Models Created

---
+++ t1/src/Models/Fornecedor.java
@@ -0,0 +1,40 @@
+package Models;
+
+public class Fornecedor {
+	private String nome;
+	private String descricao;
+	private String telefone;
+	private String eMail;
+	private Endereco endereco;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+}
---
+++ t1/src/Models/Produto.java
@@ -0,0 +1,33 @@
+package Models;
+
+public class Produto {
+	private String nome;
+	private String descricao;
+	private Fornecedor fornecedor;
+	private Estoque estoque;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	public Estoque getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(Estoque estoque) {
+		this.estoque = estoque;
+	}
+	
+}
